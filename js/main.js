45340efe71f28ab8ac4259773ef19aef632dd0ce
Cristin <tianncristiann@gmail.com>
2023-10-29 22:43:49 +0200

    add slider

--- js/main.js
+++ js/main.js
@@ -1,5 +1,9 @@
 $(function () {
-	// $('.').slick({});
+	$('.slider-blog__inner').slick({
+		dots: true,
+		prevArrow: "<button class='slick-prev'><img src='img/arrow-left.svg' alt='prev'></button>",
+		nextArrow: "<button class='slick-next'><img src='img/arrow-right.svg' alt='prev'></button>",
+	});
 	var mixer = mixitup('.portfolio__content');
 });
 
