eb73f6fd4430b854feef85199e6a691b5bf9f0e2
Cristin <tianncristiann@gmail.com>
2023-11-02 01:54:52 +0200

    addd smooth scroll

--- js/main.js
+++ js/main.js
@@ -1,9 +1,34 @@
 $(function () {
+	$('#menu a, footer__top-btn').on('click', function (event) {
+		//отменяем стандартную обработку нажатия по ссылке
+		event.preventDefault();
+
+		//забираем идентификатор бока с атрибута href
+		var id = $(this).attr('href'),
+			//узнаем высоту от начала страницы до блока на который ссылается якорь
+			top = $(id).offset().top;
+
+		//анимируем переход на расстояние - top за 1500 мс
+		$('body,html').animate({ scrollTop: top }, 1500);
+	});
+
 	$('.slider-blog__inner').slick({
 		dots: true,
 		prevArrow: "<button class='slick-prev'><img src='img/arrow-left.svg' alt='prev'></button>",
 		nextArrow: "<button class='slick-next'><img src='img/arrow-right.svg' alt='prev'></button>",
+		responsive: [
+			{
+				breakpoint: 769,
+				settings: {
+					arrows: false,
+				},
+			},
+		],
 	});
+	$('.menu__btn, .menu a').on('click', function () {
+		$('.header__top--inner').toggleClass('header__top--inner--active');
+	});
+
 	var mixer = mixitup('.portfolio__content');
 });
 
